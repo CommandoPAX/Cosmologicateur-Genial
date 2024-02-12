8b3eed657fdcb12c618a889d77b3ec46a291c4e7
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-12 11:27:43 +0100

    Imports

--- Cosmologicateur.py
+++ Cosmologicateur.py
@@ -1,2 +1,18 @@
-# Salut
+import yt
+import numpy as np
+from scipy import interpolate
+
+import density_field_library as DFL
+import Pk_library as PKL
+import MAS_library as MASL
+
+%matplotlib inline
 import matplotlib.pyplot as plt
+import matplotlib
+
+import mass_function_library as MFL
+from yt.extensions.astro_analysis.halo_analysis import HaloCatalog
+from HaloStats import halo_MF
+from colossus.cosmology import cosmology
+from colossus.lss import mass_function
+cosmology.setCosmology('planck18')
