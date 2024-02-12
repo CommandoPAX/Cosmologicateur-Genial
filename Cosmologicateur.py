59236a156aa51423f72d7276fd26e07d8ed9830e
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-12 11:54:48 +0100

    test graphe

--- Cosmologicateur.py
+++ Cosmologicateur.py
@@ -16,7 +16,7 @@ from colossus.cosmology import cosmology
 from colossus.lss import mass_function
 cosmology.setCosmology('planck18')
 
-ds=yt.load('ramses/output_00002/info_00002.txt')
+ds=yt.load('../output_00002/info_00002.txt')
 
 #plot
 q = yt.ParticlePlot(ds, 'particle_position_x', 'particle_position_y','particle_mass')
@@ -24,5 +24,5 @@ q.set_unit('particle_mass', 'Msun')
 #q.zoom(4)
 #q.annotate_timestamp(corner='upper_left', time=True, redshift=False, draw_inset_box=True,time_format='t = {time:.1f}', time_unit='code_time')
 q.annotate_scale()
-q.show()
-#q.save()
+#q.show()
+q.save()
