1e7b104225df2eb3acad624887b73a34178adf08
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-12 15:13:49 +0100

    Ajout arguments

--- Cosmologicateur.py
+++ Cosmologicateur.py
@@ -15,19 +15,44 @@ from HaloStats import halo_MF
 from colossus.cosmology import cosmology
 from colossus.lss import mass_function
 import os
+import sys, getopt
 
-import Mailateur_genial as mail
+# Utilisation :
+# python Cosmologicateur.py -i entree -o sortie
 
-cosmology.setCosmology('planck18')
 
-ds=yt.load('../output_00002/info_00002.txt')
+def main(argv):
 
-#plot
-q = yt.ParticlePlot(ds, 'particle_position_x', 'particle_position_y','particle_mass')
-q.set_unit('particle_mass', 'Msun')
-#q.zoom(4)
-#q.annotate_timestamp(corner='upper_left', time=True, redshift=False, draw_inset_box=True,time_format='t = {time:.1f}', time_unit='code_time')
-q.annotate_scale()
-#q.show()
-q.save()
+    inputfile = '../output_00002/info_00002.txt'
+    outputfile = "Resultat.png"
 
+    try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+
+    cosmology.setCosmology('planck18')
+
+    ds=yt.load(inputfile)
+
+    #plot
+    q = yt.ParticlePlot(ds, 'particle_position_x', 'particle_position_y','particle_mass')
+    q.set_unit('particle_mass', 'Msun')
+    #q.zoom(4)
+    #q.annotate_timestamp(corner='upper_left', time=True, redshift=False, draw_inset_box=True,time_format='t = {time:.1f}', time_unit='code_time')
+    q.annotate_scale()
+    #q.show()
+    q.save("Resultat.png")
+
+if __name__ == "__main__" :
+    main(sys.argv[1:])
