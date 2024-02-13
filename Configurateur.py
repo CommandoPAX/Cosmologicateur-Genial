0fa040b1f5db6cfecf2e1514a225e4e6651008fd
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-13 12:19:07 +0100

    ramses

--- Configurateur.py
+++ Configurateur.py
@@ -26,12 +26,33 @@ while 1 :
             output_monofonic+=" "
         i = 1
 
+output_ramses = ""
+while 1 :
+    ligne = ramses.readline()
+    if ligne == "" : break
+    for i in range(10): ligne = ligne.replace("  "," ")
+    ligne = ligne.split("=")
+    if ligne[0] == "levelmin" : ligne[1] = ngrid
+    if ligne[0] == "levelmiax" : ligne[1] = ngrid+6
+
+    i  = 0
+    for k in ligne :
+        output_monofonic += k
+        if not "\n" in k and not i== 0: 
+            output_monofonic+="="
+        i = 1
+
 autre = input("Changer d'autres paramètres ? (o/n)")
 
 monofonic.close()
 monofonic = open("../monofonic/monofonic.conf","w")
 monofonic.write(output_monofonic)
 monofonic.close()
+
+ramses.close()
+ramses = open("../ramses/namelist/ramses-test.nml","w")
+ramses.write(output_monofonic)
+ramses.close()
 
 if autre == "o" :
     os.system("nano ../monofonic/monofonic.conf")
