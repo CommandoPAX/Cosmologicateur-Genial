7bfe06d54f709c7fd8d09698280ba2ec7393c649
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-20 15:06:23 +0100

    algèbre

--- Configurateur.py
+++ Configurateur.py
@@ -43,6 +43,15 @@ ParticleLoad    = sc       # particle load, can be 'sc' (1x), 'bcc' (2x) or 'fcc
 
 #########################################################################################"""
 
+
+while 1 :
+    wdm = input("Matière noire chaude ? (o/n)")
+    if wdm in ("o","n") : break
+while 1:
+    ng = input("Non gaussianités ? (o/n)")
+    if ng in ("o","n") : break
+
+
 COSMOLOGY = """
 [cosmology]
 ## transfer = ... specifies the Einstein-Boltzmann plugin module
@@ -189,56 +198,9 @@ OUTPUT = """
 
 MONOFONIC = SETUP + COSMOLOGY + RANDOM + EXECUTION + OUTPUT
 
-while 1 :
-    wdm = input("Matière noire chaude ? (o/n)")
-    if wdm in ("o","n") : break
-while 1:
-    ng = input("Non gaussianités ? (o/n)")
-    if ng in ("o","n") : break
 
-while 1 :
-    type_mono = input("Utiliser quelle version de monofonic ? (cdm/wdm)")
-    if type_mono in ("cdm","wdm") : break
-
-if 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if type_mono == "cdm" :
-    monofonic= open("../monofonic/monofonic.conf","r")
-else :
-    monofonic = open("../monofonic_exp/PNG/NG.conf","r")
-
-output_monofonic = ""
-while 1 :
-    ligne = monofonic.readline()
-    if ligne == "" : break
-    for i in range(10): ligne = ligne.replace("  "," ")
-    ligne = ligne.split(" ")
-    if ligne[0] == "GridRes" : ligne[2] = str(2**int(ngrid))
-    if ligne[0] == "BoxLength" : ligne[2] = taille
-
-    i  = 0
-    for k in ligne :
-        output_monofonic += k
-        if not "\n" in k and not i== 0: 
-            output_monofonic+=" "
-        i = 1
-monofonic.close()
+monofonic = open("../monofonic_exp/config.conf","w")
+monofonic.write(MONOFONIC)
 
 
 output_ramses = ""
@@ -254,51 +216,30 @@ while 1 :
         output_ramses += k
         if not "\n" in k : 
             output_ramses+="="
-if type_mono == "wdm" : output_ramses = output_ramses.replace("initfile(1)='monofonic/","initfile(1)='monofonic_exp/")
-else : output_ramses = output_ramses.replace("initfile(1)='monofonic_exp/","initfile(1)='monofonic/")
+#if type_mono == "wdm" : output_ramses = output_ramses.replace("initfile(1)='monofonic/","initfile(1)='monofonic_exp/")
+#else : output_ramses = output_ramses.replace("initfile(1)='monofonic_exp/","initfile(1)='monofonic/")
 
 ramses.close()
 
-output_monofonic = output_monofonic.replace("="," = ")
-output_monofonic = output_monofonic.replace("#"," # ")
-output_monofonic = output_monofonic.replace("#  #"," # ")
-
 autre = input("Changer d'autres paramètres ? (o/n)")
 
-if type_mono == "cdm" :
-    monofonic= open("../monofonic/monofonic.conf","w")
-else :
-    monofonic = open("../monofonic_exp/PNG/NG.conf","w")
-
-monofonic.write(output_monofonic)
-monofonic.close()
 
 ramses = open("../ramses/namelist/ramses.nml","w")
 ramses.write(output_ramses)
 ramses.close()
 
 if autre == "o" :
-    if type_mono == "cdm" :
-        os.system("nano ../monofonic/monofonic.conf")
-    else :
-        os.system("nano ../monofonic_exp/PNG/NG.conf")
-
+    os.system("nano ../monofonic_exp/config.conf")
     os.system("nano ../ramses/namelist/ramses.nml")
 
-if type_mono == "cdm":
-    os.system("rm -fr ../monofonic/build")
-    os.system("mkdir ../monofonic/build")
-    os.system("cd ../monofonic/build ; cmake ..")
-    os.system("cd ../monofonic/build ; make")
-    os.system("cd ../monofonic; build/monofonIC monofonic.conf")
-    os.system("cp ../monofonic/ics_ramses/ic_poscx ../monofonic/ics_ramses/ic_deltab")
-else :
-    os.system("rm -fr ../monofonic_exp/build")
-    os.system("mkdir ../monofonic_exp/build")
-    os.system("cd ../monofonic_exp/build ; cmake .." )
-    os.system("cd ../monofonic_exp/build ; make")
-    os.system("cd ../monofonic_exp; build/monofonIC ./PNG/NG.conf")
-    os.system("cp ../monofonic_exp/ics_ramses/ic_poscx ../monofonic_exp/ics_ramses/ic_deltab")
+
+os.system("rm -fr ../monofonic_exp/build")
+os.system("mkdir ../monofonic_exp/build")
+os.system("cd ../monofonic_exp/build ; cmake .." )
+os.system("cd ../monofonic_exp/build ; make")
+os.system("cd ../monofonic_exp; build/monofonIC config.conf")
+os.system("cp ../monofonic_exp/ics_ramses/ic_poscx ../monofonic_exp/ics_ramses/ic_deltab")
 os.system("cd ../ramses/bin; make clean")
 os.system("cd ../ramses/bin; make NDIM=3")
+
 #os.system("../ramses/bin/ramses3d ../ramses/namelist/ramses.nml")  le fichier bash le fait déjà
