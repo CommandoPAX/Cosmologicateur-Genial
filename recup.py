f384325588d48625b25a3e1dbe569e6600128a7b
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-02-27 14:44:47 +0100

    recup

--- recup.py
+++ recup.py
@@ -1,14 +1,14 @@
 import os
-import sys
+import sys,getopt
 
 def Recup(argv):
-    opts, args = getopt.getopt(argv,"goa")
+    opts, args = getopt.getopt(argv,"ga")
     
     for opt, arg in opts:
-        if opt in ("-g","-a"):
-            os.system()
-        if opt in ("-o","-a"):
-            os.system()
+        if opt in ("-g"):
+            os.system("scp -r tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/RESULT/*/*.png ~/RESULT/")
+        if opt in ("-o"):
+            os.system("scp -r \"tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/RESULT/* ~/RESULT/")
 
 
     os.system()
