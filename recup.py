a737e0f32d52ba24c342c37cf590cfac2534f780
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-03-11 10:03:26 +0100

    ident

--- recup.py
+++ recup.py
@@ -15,22 +15,26 @@ def Recup(argv):
         if opt in ("-g"):
             p = Popen(['scp', '-r', 'tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/RESULT/*/*.png','~/RESULT/'], stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
 
-
+            prompt = p.communicate("gaeK6Oafai1eN6e" + '\n')
+            output = prompt[0]
+            print(output)
             #os.system("scp -r tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/RESULT/*/*.png ~/RESULT/")
         if opt in ("-a"):
             #os.system("scp -r \"tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/RESULT/*\" ~/RESULT/")
         
             p = Popen(['scp', '-r', '\"tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/RESULT/*\"','~/RESULT/'], stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
-
+            prompt = p.communicate("gaeK6Oafai1eN6e" + '\n')
+            output = prompt[0]
+            print(output)
         
         if opt in ("-c"):
             p = Popen(['scp', '-r', '\"tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/*.py\"','~/Cosmologicateur-genial/'], stdin=PIPE, stderr=PIPE, stdout=PIPE, text=True)
-
+            prompt = p.communicate("gaeK6Oafai1eN6e" + '\n')
+            output = prompt[0]
+            print(output)
             #os.system("scp -r \"tbruant@obas-hpc.astro.unistra.fr:~/Cosmologicateur-Genial/*.py\" ~/Cosmologicateur-genial/")
         
-        prompt = p.communicate("gaeK6Oafai1eN6e" + '\n')
-        output = prompt[0]
-        print(output)
+
 
 if __name__=="__main__":
     Recup(sys.argv[1:])
