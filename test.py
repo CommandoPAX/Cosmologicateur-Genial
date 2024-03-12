517019ce5c834c4217970c82e6684aa60855e9d0
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-03-12 14:01:30 +0100

    test

--- test.py
+++ test.py
@@ -14,6 +14,14 @@ import os, sys, getopt
 import json, datetime 
 from Errorateur import LogError
 
-ds = yt.load("./output_00001/info_00001.txt")
+ds = yt.load("../test/output_00002/info_00002.txt")
 print(ds.all_data())
-yt.ParticlePlot(ds, 'particle_position_x', 'particle_position_y','particle_mass')
+plot = yt.ParticlePlot(ds, 'particle_position_x', 'particle_position_y','particle_mass')
+plot.save("test.pdf")
+os.system("evince test.pdf")
+
+fichier = open("../fields","r")
+while 1:
+    l = fichier.readline()
+    if "’, ‘" in l : print(l)
+    if l == "" : break
