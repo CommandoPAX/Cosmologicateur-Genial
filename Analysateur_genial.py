fbd293d2fb664e563b59c3c2237baeee154c52b4
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-03-18 16:17:03 +0100

    Et je peux prendre phi tilda à la moins 1

--- Analysateur_genial.py
+++ Analysateur_genial.py
@@ -257,6 +257,6 @@ if __name__ == "__main__" :
     print(Pow[0]-Pow[1]-Pow[2])
     Plot_Pow(Pow[0], lcdm, labelname = "WDM3PGN1000/lcdm")
     Plot_Pow(Pow[1] + Pow[2], lcdm, labelname = "(WDM3+PGN1000)/lcdm")
-    Plot_Pow(Pow[0] - (Pow[1] + Pow[2]), lcdm)
+    Plot_Pow(-Pow[0] + (Pow[1] + Pow[2]), lcdm)
     plt.savefig("./RESULT/RATIOOOOOO")
     Plot_sigma_8()                
