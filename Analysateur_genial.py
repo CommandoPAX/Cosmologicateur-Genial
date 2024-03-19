204e4fb6d041150b24574832a4009b53aca5a648
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-03-19 10:01:06 +0100

    zoom

--- Analysateur_genial.py
+++ Analysateur_genial.py
@@ -137,7 +137,7 @@ def Diviser_Pow (Simu1, Simu2) :
     plt.loglog(Simu1["k"],Simu1["Pk0"]/Simu2["Pk0"],label="Ratio "+Simu1.name+" / "+Simu2.name) #plot measure from N-body
     axes = plt.gca()
     axes.set_xlim(2e-2,0.9)
-    axes.set_ylim(0.95,1.05)
+    axes.set_ylim(0.98,1.02)
     plt.xlabel("k [h/Mpc]")
     plt.ylabel(r"P(k) [$(Mpc/h)^3$]")
     plt.legend()
