d4bb97bb6b0a6d90db0ac823eaa608b008db9eab
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-03-21 11:34:28 +0100

    corr

--- Analysateur_genial.py
+++ Analysateur_genial.py
@@ -383,7 +383,7 @@ if __name__ == "__main__" :
     plt.title("z = 1")
 
     Plot_Pow(pow_pw2, lcdm2, labelname = "(m = 500 ev, fnl = 1000)/lcdm")
-    Plot_Pow(pow_p2+ pow_w2, lcdm2, labelname = "((m = 500 fnl = 0) + (m = 0 fnl = 500))/lcdm")
+    Plot_Pow((pow_p2+ pow_w2)/2, lcdm2, labelname = "((m = 500 fnl = 0) + (m = 0 fnl = 1000))/lcdm")
     Plot_Pow(-pow_pw2 + (pow_p2 + pow_w2)/2, lcdm, labelname= "Différence")
 
     plt.savefig("./RESULT/Superposition wdm500fnl1000 - z=1 .png")
@@ -393,7 +393,7 @@ if __name__ == "__main__" :
     plt.title("z = 0")
 
     Plot_Pow(pow_pw3, lcdm, labelname = "(m = 500 ev, fnl = 1000)/lcdm")
-    Plot_Pow(pow_p3+ pow_w3, lcdm, labelname = "((m = 500 fnl = 0) + (m = 0 fnl = 500))/lcdm")
+    Plot_Pow((pow_p3+ pow_w3)/2, lcdm, labelname = "((m = 500 fnl = 0) + (m = 0 fnl = 1000))/lcdm")
     Plot_Pow(-pow_pw3 + (pow_p3 + pow_w3)/2, lcdm, labelname= "Différence")
 
     plt.savefig("./RESULT/Superposition wdm500fnl1000 - z=0 .png")
