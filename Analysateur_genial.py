2522138de3ba4860b175eaefe6c84d1360ed8712
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-03-21 12:48:37 +0100

    le reste aussi

--- Analysateur_genial.py
+++ Analysateur_genial.py
@@ -12,9 +12,9 @@ import matplotlib
 import re 
 import MAS_library as MASL
 
-matplotlib.rcParams.update({'font.size': 20})
+#matplotlib.rcParams.update({'font.size': 20})
 #matplotlib.rcParams['text.usetex'] = True
-matplotlib.rcParams["figure.facecolor"]='w'
+#matplotlib.rcParams["figure.facecolor"]='w'
 #matplotlib.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
 
 plt.tight_layout()
