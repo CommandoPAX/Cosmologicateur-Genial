c65638863f68ce6ac8c1b3ff3bb40129d10714f7
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-03-21 16:17:52 +0100

    test

--- Analysateur_genial.py
+++ Analysateur_genial.py
@@ -396,6 +396,7 @@ def superposer (fnl, wdm, path_lcdm = "./RESULT/2024-03-12 20:07:08 - LCDM"):
     axes[0].set_ylim(pow_pw2[160],1.05)
 
     axes[1].set_xlim(0.1,2)
+    axes[1].set_ylim(pow_pw2 / (pow_p2 * pow_w2)[9],pow_pw2 / (pow_p2 * pow_w2)[160])
 
     axes[1].set_xlabel("k [h/Mpc]")
     axes[0].set_ylabel(r"P(k) [$(Mpc/h)^3$]")
@@ -426,6 +427,8 @@ def superposer (fnl, wdm, path_lcdm = "./RESULT/2024-03-12 20:07:08 - LCDM"):
     axes[1].set_xlabel("k [h/Mpc]")
     axes[0].set_ylabel(r"P(k) [$(Mpc/h)^3$]")
     axes[1].set_ylabel(r"P(k) [$(Mpc/h)^3$]")
+    axes[1].set_ylim(pow_pw3 / (pow_p3 * pow_w3)[9],pow_pw3 / (pow_p3 * pow_w3)[160])
+
 
 
     plt.title("z = 0")
