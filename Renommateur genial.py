a8633a51b1e71ff71bf1cc84ccaf518e6e470ab4
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-04-02 10:19:13 +0200

    test

--- Renommateur genial.py
+++ Renommateur genial.py
@@ -5,5 +5,20 @@ for root, dirs, files in os.walk("./RESULT/"):
     for dir in dirs :
 
         for root2,dirs2,files in os.walk("./RESULT/"+dir):
+
+            if "output_00004" in dirs2 :
+                os.system("mv "+ dir+"/output_00004 "+ dir+"/output_00002" )
+                os.system("mv "+ dir+"/output_00005 "+ dir+"/output_00003" )
+            elif "output_00006" in dirs2 and not "output_00004" in dirs2:
+                os.system("mv "+ dir+"/output_00005 "+ dir+"/output_00002" )
+                os.system("mv "+ dir+"/output_00006 "+ dir+"/output_00003" )
+
+            
+for root, dirs, files in os.walk("./RESULT/"):
+
+    for dir in dirs :
+
+        for root2,dirs2,files in os.walk("./RESULT/"+dir):
+
             for dir2 in dirs2 :
                 print (dir+" -> "+dir2)
