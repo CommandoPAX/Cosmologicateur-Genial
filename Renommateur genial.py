26144a106dd8dea58659ddeea80e788001fcf903
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-04-02 11:24:08 +0200

    test

--- Renommateur genial.py
+++ Renommateur genial.py
@@ -4,14 +4,52 @@ for root, dirs, files in os.walk("./RESULT/"):
 
     for dir in dirs :
 
-        for root2,dirs2,files in os.walk("./RESULT/"+dir):
+        for root2,dirs2,files2 in os.walk("./RESULT/"+dir):
 
-            if "output_00005" in dirs2 :
-                os.system("rm -r \"./RESULT/"+ dir+"/output_00002\"")
-                os.system("mv \"./RESULT/"+ dir+"/output_00003\" \"./RESULT/"+ dir+"/output_00002\"" )
-                os.system("mv \"./RESULT/"+ dir+"/output_00005\" \"./RESULT/"+ dir+"/output_00003\"" )
-           
-            
+            max = 0
+
+            for i in range(7):
+                for file in files2 :
+                    if str(i)+"_" in file and i > max :
+                        max = i
+
+            if max == 6 :
+                for file in files2 :
+                    if "2_" in file or "3_" in file or "4_" in file :
+                        os.system("rm ./RESULT/"+dir+"/"+file)
+
+                for file in files2 :
+                    if "6_" in file :
+                        nouveau = "3_"+file[2:]
+                        os.system("mv "+file+ " "+nouveau)
+                    if "5_" in file :
+                        nouveau = "2_"+file[2:]
+                        os.system("mv "+file+ " "+nouveau)
+
+            elif max == 5 :
+                for file in files2:
+                    if "2_" in file or "3_" in file  :
+                        os.system("rm ./RESULT/"+dir+"/"+file)
+                for file in files2 :
+                    if "5_" in file :
+                        nouveau = "3_"+file[2:]
+                        os.system("mv "+file+ " "+nouveau)
+                    if "4_" in file :
+                        nouveau = "2_"+file[2:]
+                        os.system("mv "+file+ " "+nouveau)
+            elif max == 4 :
+                for file in files2:
+                    if "2_" in file :
+                        os.system("rm ./RESULT/"+dir+"/"+file)
+                for file in files2 :
+                    if "4_" in file :
+                        nouveau = "3_"+file[2:]
+                        os.system("mv "+file+ " "+nouveau)
+                    if "3_" in file :
+                        nouveau = "2_"+file[2:]
+                        os.system("mv "+file+ " "+nouveau)
+
+
 for root, dirs, files in os.walk("./RESULT/"):
 
     for dir in dirs :
