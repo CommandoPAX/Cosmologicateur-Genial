7a36b08f7016208d72a4792961bfa3a597d68fa0
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-04-02 10:23:25 +0200

    test

--- Renommateur genial.py
+++ Renommateur genial.py
@@ -7,11 +7,11 @@ for root, dirs, files in os.walk("./RESULT/"):
         for root2,dirs2,files in os.walk("./RESULT/"+dir):
 
             if "output_00004" in dirs2 :
-                os.system("mv /"+ dir+"/output_00004 /"+ dir+"/output_00002" )
-                os.system("mv /"+ dir+"/output_00005 /"+ dir+"/output_00003" )
+                os.system("mv ./RESULT/"+ dir+"/output_00004 ./RESULT/"+ dir+"/output_00002" )
+                os.system("mv ./RESULT/"+ dir+"/output_00005 ./RESULT/"+ dir+"/output_00003" )
             elif "output_00006" in dirs2 and not "output_00004" in dirs2:
-                os.system("mv /"+ dir+"/output_00005 /"+ dir+"/output_00002" )
-                os.system("mv /"+ dir+"/output_00006 /"+ dir+"/output_00003" )
+                os.system("mv ./RESULT/"+ dir+"/output_00005 ./RESULT/"+ dir+"/output_00002" )
+                os.system("mv ./RESULT/"+ dir+"/output_00006 ./RESULT/"+ dir+"/output_00003" )
 
             
 for root, dirs, files in os.walk("./RESULT/"):
