8a7107958f7825c7621f65783fea1f553d990a3d
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-04-02 10:30:44 +0200

    5->3

--- Renommateur genial.py
+++ Renommateur genial.py
@@ -6,14 +6,11 @@ for root, dirs, files in os.walk("./RESULT/"):
 
         for root2,dirs2,files in os.walk("./RESULT/"+dir):
 
-            if "output_00004" in dirs2 :
-                os.system("mv \"./RESULT/"+ dir+"/output_00004\" \"./RESULT/"+ dir+"/output_00002\"" )
+            if "output_00005" in dirs2 :
+                os.system("rm -r \"./RESULT/"+ dir+"/output_00002\"")
+                os.system("mv \"./RESULT/"+ dir+"/output_00003\" \"./RESULT/"+ dir+"/output_00002\"" )
                 os.system("mv \"./RESULT/"+ dir+"/output_00005\" \"./RESULT/"+ dir+"/output_00003\"" )
-            elif "output_00006" in dirs2 and not "output_00004" in dirs2:
-                #print("mv ./RESULT/"+ dir+"/output_00005 ./RESULT/"+ dir+"/output_00002")
-                os.system("mv \"./RESULT/"+ dir+"/output_00005\" \"./RESULT/"+ dir+"/output_00002\"" )
-                os.system("mv \"./RESULT/"+ dir+"/output_00006\" \"./RESULT/"+ dir+"/output_00003\"" )
-
+           
             
 for root, dirs, files in os.walk("./RESULT/"):
 
