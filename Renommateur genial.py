8ceb8d95729c5a6761aa642f36ad8e1029537e21
casse-stylo <fabien.castillo@etu.unistra.fr>
2024-04-02 10:27:26 +0200

    test

--- Renommateur genial.py
+++ Renommateur genial.py
@@ -7,12 +7,12 @@ for root, dirs, files in os.walk("./RESULT/"):
         for root2,dirs2,files in os.walk("./RESULT/"+dir):
 
             if "output_00004" in dirs2 :
-                os.system("mv ./RESULT/"+ dir+"/output_00004 ./RESULT/"+ dir+"/output_00002" )
-                os.system("mv ./RESULT/"+ dir+"/output_00005 ./RESULT/"+ dir+"/output_00003" )
+                os.system("mv \"./RESULT/"+ dir+"/output_00004\" \"./RESULT/"+ dir+"/output_00002\"" )
+                os.system("mv \"./RESULT/"+ dir+"/output_00005\" \"./RESULT/"+ dir+"/output_00003\"" )
             elif "output_00006" in dirs2 and not "output_00004" in dirs2:
-                print("mv ./RESULT/"+ dir+"/output_00005 ./RESULT/"+ dir+"/output_00002")
-                os.system("mv ./RESULT/"+ dir+"/output_00005 ./RESULT/"+ dir+"/output_00002" )
-                os.system("mv ./RESULT/"+ dir+"/output_00006 ./RESULT/"+ dir+"/output_00003" )
+                #print("mv ./RESULT/"+ dir+"/output_00005 ./RESULT/"+ dir+"/output_00002")
+                os.system("mv \"./RESULT/"+ dir+"/output_00005\" \"./RESULT/"+ dir+"/output_00002\"" )
+                os.system("mv \"./RESULT/"+ dir+"/output_00006\" \"./RESULT/"+ dir+"/output_00003\"" )
 
             
 for root, dirs, files in os.walk("./RESULT/"):
@@ -22,4 +22,4 @@ for root, dirs, files in os.walk("./RESULT/"):
         for root2,dirs2,files in os.walk("./RESULT/"+dir):
 
             for dir2 in dirs2 :
-                pass#print (dir+" -> "+dir2)
+                print (dir+" -> "+dir2)
