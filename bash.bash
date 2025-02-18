b6181cc5f607be872b45b076e659a8c0bf34dbb9
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-18 12:21:50 +0100

    module load

--- bash.bash
+++ bash.bash
@@ -8,6 +8,9 @@
 #SBATCH --mail-user=fabien.castillo@etu.unistra.fr
 #SBATCH --mail-type=END,FAIL 
 
+module purge
+module load intelpython
+
 mpirun -np 16 python /home/fcastillo/Cosmologicateur-Genial/Cosmologicateur.py
 
 exit 0
