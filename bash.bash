4b47d866bb6832c6868782d286cc9e7a93dd4e91
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-18 11:27:38 +0100

    bash

---
+++ bash.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=yt
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=10:00:00
+#SBATCH --output=/home/fcastillo
+#SBATCH --mail-user=fabien.castillo@etu.unistra.fr
+#SBATCH --mail-type=END,FAIL 
+
+mpirun -np 16 python /home/fcastillo/Cosmologicateur-Genial/Cosmologicateur.py
+
+exit 0
