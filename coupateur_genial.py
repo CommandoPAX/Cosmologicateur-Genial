2141d422af1a79d9c770b43c7719126e1fec1b04
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-21 20:57:32 +0100

    n -> N

--- coupateur_genial.py
+++ coupateur_genial.py
@@ -2,8 +2,8 @@ import numpy as np
 from astropy.io import fits
 import sys
 
-n = sys.argv[1]
-i = sys.argv[2]
+n = int(sys.argv[1])
+i = int(sys.argv[2])
 
 pre = "../../../data100/fcastillo/RESULT/"
 snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
@@ -13,7 +13,7 @@ hdul = fits.open(data)
 delta = hdul[0].data
 hdul.close()
 
-n = np.shape(delta)[0]
+N = np.shape(delta)[0]
 
 header = fits.Header()
 header['COMMENT'] = 'Champ de densite'
@@ -24,14 +24,14 @@ header['NAXIS3'] = delta.shape[2] /2
 
 for j in range(8):
  
-    if j == 0 :_data = delta[0:n//2, 0:n//2, 0:n//2]
-    if j == 1 :_data = delta[0:n//2, 0:n//2, n//2:n]
-    if j == 2 :_data = delta[0:n//2, n//2:n, 0:n//2]
-    if j == 3 :_data = delta[0:n//2, n//2:n, n//2:n]
-    if j == 4 :_data = delta[n//2:n, 0:n//2, 0:n//2]
-    if j == 5 :_data = delta[n//2:n, 0:n//2, n//2:n]
-    if j == 6 :_data = delta[n//2:n, n//2:n, 0:n//2]
-    if j == 7 :_data = delta[n//2:n, n//2:n, n//2:n]
+    if j == 0 :_data = delta[0:N//2, 0:N//2, 0:N//2]
+    if j == 1 :_data = delta[0:N//2, 0:N//2, N//2:N]
+    if j == 2 :_data = delta[0:N//2, N//2:N, 0:N//2]
+    if j == 3 :_data = delta[0:N//2, N//2:N, N//2:N]
+    if j == 4 :_data = delta[N//2:N, 0:N//2, 0:N//2]
+    if j == 5 :_data = delta[N//2:N, 0:N//2, N//2:N]
+    if j == 6 :_data = delta[N//2:N, N//2:N, 0:N//2]
+    if j == 7 :_data = delta[N//2:N, N//2:N, N//2:N]
 
 
     hdu = fits.PrimaryHDU(data=delta, header=header)
