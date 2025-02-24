40a9445139b7d22df07b4742b9e37608bf509c27
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-24 11:12:04 +0100

    imshow

--- test_min.py
+++ test_min.py
@@ -9,29 +9,23 @@ print(sys.path)
 
 data = np.random.normal(size=(64,64,64))
 
-plt.imshow(data[0])
-plt.show()
 
 v0,v1,v2,v3 = calculateMFs(data)
 
-plt.figure()
 
 plt.plot(v0,label="v0")
 plt.legend()
 
-plt.figure()
 
 plt.plot(v1,label="v1")
 plt.legend()
 
-plt.figure()
 
 plt.plot(v2,label="v2")
 plt.legend()
 
-plt.figure()
 
 plt.plot(v3,label="v3")
 
 plt.legend()
-plt.show()
+plt.savefig("test_min.png")
