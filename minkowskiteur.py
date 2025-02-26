12c957ce0087b9db188926ce6838dba917bf993a
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-26 16:36:28 +0100

    out

--- minkowskiteur.py
+++ minkowskiteur.py
@@ -9,13 +9,13 @@ snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminu
 
 fichier = open("mse.sh","w")
 fichier.write(f"""#!/bin/bash
-#SBATCH --job-name=disperse
+#SBATCH --job-name=minkowski
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
 #SBATCH --ntasks-per-node=32
 #SBATCH --mem=100gb
 #SBATCH --time=24:00:00
-#SBATCH --output=/home/fcastillo/disperse.out
+#SBATCH --output=/home/fcastillo/minkowski.out
 #SBATCH --mail-user=fabien.castillo@etu.unistra.fr
 #SBATCH --mail-type=ALL
 #SBATCH --partition=pscomp
