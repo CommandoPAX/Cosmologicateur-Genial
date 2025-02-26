1369dc5664310ce208ecc07d1868b384976079a8
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-26 14:57:26 +0100

    maj

--- dispersateur_tout.py
+++ dispersateur_tout.py
@@ -3,13 +3,13 @@ import sys
 from astropy.io import fits
 import numpy as np
 
-pre = "../../../data100/fcastillo/RESULT/"
+pre = "/data100/fcastillo/RESULT/"
 snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
 
 
 for n in range(6):
     for i in range(5):
-        fichier = open("mse_"+str(n)+"_"+str(i)+".bash","w")
+        fichier = open("mse_"+str(n)+"_"+str(i)+".sh","w")
 
         input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
 
@@ -20,9 +20,9 @@ for n in range(6):
 #SBATCH --ntasks-per-node=16
 #SBATCH --mem=200gb
 #SBATCH --time=2:00:00
-#SBATCH --output=/home/fcastillo/disperse_{n}_{i}.out   
+#SBATCH --output=/home/fcastillo/disperse_{n}_{i}.out
 #SBATCH --mail-user=fabien.castillo@etu.unistra.fr
-#SBATCH --mail-type=ALL 
+#SBATCH --mail-type=ALL
 #SBATCH --partition=pscomp""")
         fichier.write("""
 
@@ -30,7 +30,7 @@ module () {
   eval $(/usr/bin/modulecmd bash $*)
 }
 
-source /etc/profile.d/modules.sh 
+source /etc/profile.d/modules.sh
 """)
 
         fichier.write(f"""
@@ -43,4 +43,4 @@ module load disperse/0.9.24
 exit 0""")
         
         fichier.close()
-        os.system("sbatch mse_"+str(n)+"_"+str(i)+".bash")
+        os.system("sbatch mse_"+str(n)+"_"+str(i)+".sh")
