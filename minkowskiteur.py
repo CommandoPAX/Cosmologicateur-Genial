82e66812d5dea665d08dd98c1b06458cebc18582
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-27 09:52:04 +0100

    defusion

--- minkowskiteur.py
+++ minkowskiteur.py
@@ -7,20 +7,26 @@ import time
 pre = "/data100/fcastillo/RESULT/"
 snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
 
-fichier = open("minkowski.sh","w")
-fichier.write(f"""#!/bin/bash
-#SBATCH --job-name=minkowski
+for n in range(6):
+    for i in range(5):
+
+        input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
+
+
+        fichier = open(f"minkowski_{n}_{i}.sh","w")
+        fichier.write(f"""#!/bin/bash
+#SBATCH --job-name=minkowski_{n}_{i}
 #SBATCH --nodes=1
 #SBATCH --ntasks=32
 #SBATCH --ntasks-per-node=32
-#SBATCH --mem=500gb
-#SBATCH --time=48:00:00
-#SBATCH --output=/home/fcastillo/minkowski.out
+#SBATCH --mem=100gb
+#SBATCH --time=2:00:00
+#SBATCH --output=/home/fcastillo/minkowski_{n}_{i}.out
 #SBATCH --mail-user=fabien.castillo@etu.unistra.fr
 #SBATCH --mail-type=ALL
 #SBATCH --partition=pscomp
 """)
-fichier.write("""
+        fichier.write("""
 
 module () {
 eval $(/usr/bin/modulecmd bash $*)
@@ -29,21 +35,17 @@ eval $(/usr/bin/modulecmd bash $*)
 source /etc/profile.d/modules.sh
 """)
 
-fichier.write(f"""
+        fichier.write(f"""
 module purge
 module load intelpython
 module load inteloneapi/2025.0.1
 """)
 
-for n in range(6):
-    for i in range(5):
-
-        input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
 
         fichier.write(f"""
 python minkowski.py {n} {i}
 """)
             
-fichier.write("exit 0")
-fichier.close()
-os.system("sbatch minkowski.sh")
+        fichier.write("exit 0")
+        fichier.close()
+        os.system(f"sbatch minkowski_{n}_{i}.sh")
