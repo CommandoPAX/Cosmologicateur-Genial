9e87ba08c8eac4dd90ab66d6d836b7b34dc349d3
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-27 10:06:24 +0100

    dispersateur

--- dispersateur_tout.py
+++ dispersateur_tout.py
@@ -6,42 +6,47 @@ import time
 
 pre = "/data100/fcastillo/RESULT/"
 snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
-
-fichier = open("mse.sh","w")
-fichier.write(f"""#!/bin/bash
-#SBATCH --job-name=disperse
-#SBATCH --mem=500gb
-#SBATCH --time=48:00:00
-#SBATCH --output=/home/fcastillo/disperse.out
-#SBATCH --mail-user=fabien.castillo@etu.unistra.fr
-#SBATCH --mail-type=ALL
-#SBATCH --partition=pscomp
-""")
-fichier.write("""
-
-module () {
-eval $(/usr/bin/modulecmd bash $*)
-}
-
-source /etc/profile.d/modules.sh
-""")
-
-fichier.write(f"""
-module purge
-module load disperse/0.9.24
-""")
-
 for n in range(6):
     for i in range(5):
 
         input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
 
-        fichier.write(f"""
-/softs/disperse/0.9.24/bin/mse {input_}_0.fits -cut 1 -upSkl -manifolds -outName {pre+snapshots[n]+"/"+str(i)+"_densite_0.fits"} -periodicity 0 -forceLoops
-/softs/disperse/0.9.24/bin/skelconv {pre+snapshots[n]+"/"+str(i)+"_densite_0.fits_c1.up.NDskl"} -smooth 1 -outName {pre+snapshots[n]+"/"+str(i)+"_densite_0.fits_c1.up.NDskl"} -to NDskl_ascii
 
+        fichier = open("mse.sh","w")
+        fichier.write(f"""#!/bin/bash
+#SBATCH --job-name=skl2_{n}_{i}
+#SBATCH --partition=pscomp
+#SBATCH --mem=100gb
+#SBATCH --time=2:00:00
+#SBATCH --output=skl2_{n}_{i}_%j.out
+#SBATCH --mail-user=fabien.castillo@etu.unistra.fr
+#SBATCH --mail-type=ALL
 """)
+        fichier.write("""
+
+module () {
+  eval $(/usr/bin/modulecmd bash $*)
+}
+
+source /etc/profile.d/modules.sh
+module purge
+module load disperse/0.9.24""")
+
+
+        fichier.write(f"""
+path_dat="{pre+snapshots[n]}/"
+path="{pre+snapshots[n]}/"
+
+file="{i}_densite_0"
+
+persistence=0.1
+smoothing=1
+
+/softs/disperse/0.9.24/bin/mse $path_dat$file".fits" -cut $persistence -upSkl -manifolds -outName $path$file -periodicity 0 -forceLoops
+
+/softs/disperse/0.9.24/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -smooth $smoothing -to NDskl_ascii
+
+exit 0""")
             
-fichier.write("exit 0")
-fichier.close()
-os.system("sbatch mse.sh")
+        fichier.close()
+        os.system("sbatch mse.sh")
---
+++ job_skel_grid_2.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#SBATCH --job-name=skl2
+#SBATCH --partition=pscomp
+#SBATCH --mem=100gb
+#SBATCH --time=48:00:00
+#SBATCH --output=skl2_2_%j.out
+#SBATCH --mail-user=fabien.castillo@etu.unistra.fr
+#SBATCH --mail-type=ALL
+#SBATCH --ntasks=32
+#SBATCH --ntasks-per-node=32
+
+module () {
+  eval $(/usr/bin/modulecmd bash $*)
+}
+
+source /etc/profile.d/modules.sh
+module purge
+module load disperse/0.9.24
+
+path_dat="/data100/fcastillo/RESULT/benchM/"
+path="/data100/fcastillo/RESULT/benchM/"
+
+file="4_densite_0"
+
+persistence=1
+smoothing=1
+
+
+/softs/disperse/0.9.24/bin/mse $path_dat$file".fits" -cut $persistence -nthreads 32 -upSkl -manifolds -outName $path$file -periodicity 0 -forceLoops
+
+
+#/softs/disperse/0.9.24/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -smooth $smoothing -to segs_ascii
+#/softs/disperse/0.9.24/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -smooth $smoothing -to NDskl_ascii
+
+##/softs/disperse/0.9.24/bin/skelconv $path$file"_s"$persistence".up.NDskl" -outName $path$file"_s"$persistence".up.NDskl" -breakdown -smooth $smoothing
+
+
+##/softs/disperse/0.9.24-CentOS6/bin/mse $path_dat$file".ND" -nthreads 40 -cut $persistence -upSkl -manifolds -outName $path$file -periodicity 0 -forceLoops
+##/softs/disperse/0.9.24-CentOS6/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -breakdown -smooth $smoothing
+##/softs/disperse/0.9.24-CentOS6/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -breakdown
+##/softs/disperse/0.9.24-CentOS6/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -smooth $smoothing
+##/softs/disperse/0.9.24-CentOS6/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -breakdown -smooth $smoothing
+##/softs/disperse/0.9.24-CentOS6/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -breakdown -smooth $smoothing -to vtp
+##/softs/disperse/0.9.24-CentOS6/bin/mse $path_dat$file".ND" -loadMSC $path$file".MSC" -cut $persistence -outName $path$file -dumpManifolds J1a -forceLoops
+
+
+
+exit 0
