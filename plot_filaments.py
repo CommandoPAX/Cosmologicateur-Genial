7b2b41beae88ca069eed855a76b1039dc910539e
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-28 09:55:02 +0100

    mediane

--- plot_filaments.py
+++ plot_filaments.py
@@ -41,14 +41,17 @@ if __name__ == "__main__" :
                 label = labels[j]
 
                 try :
-                    hist = np.load(f"/data100/fcastillo/RESULT/{snapshots[j]}/{i}_densite_0_c0.1_len_fil.txt.npy")
+                    longueurs = np.load(f"/data100/fcastillo/RESULT/{snapshots[j]}/{i}_densite_0_c0.1_len_fil.txt.npy")
+
+                    hist = np.histogram(longueurs, density= True, range = [0, 10], bins=20)
+                    hist = hist[0]
                     
                     axes.plot(hist, color= couleur, ls = ls, label=label)
                     axes.set_xlabel("log longueur [Mpc / h]")
                     plt.xscale("log")
                     axes.set_ylabel("Probabilite")
                     axes.set_ylim(0,0.35)
-                    #axes.axvline(np.median(np.array(longueurs)), color= couleur, ls = ls)
+                    axes.axvline(np.median(longueurs), color= couleur, ls = ls)
                 except:
                      print(j, i)
                     
