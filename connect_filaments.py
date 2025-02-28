9ff76fed048e503e74168e7e4b84b4a470684e43
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-28 12:42:05 +0100

    connection

---
+++ connect_filaments.py
@@ -0,0 +1,18 @@
+from skeletonnateur_hpc import*
+import sys
+import numpy as np
+
+n = int(sys.argv[1])
+i = int(sys.argv[2])
+
+snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
+labels = ["LCDM", "fnl = -500", "m = 500 eV", "WDM & fnl = -500", "fnl = 500", "WDM & fnl = 500"]
+
+
+squelette = Squelette_3d(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1.up.NDskl.S001.a.NDskl")
+
+n = []
+for fil in squelette.liste_filaments :
+    n.append(len(fil.data_samp))
+
+np.save(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1_connect_fil.txt", np.array(n))
---
+++ connecteur.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from astropy.io import fits
+import numpy as np
+import time
+
+pre = "/data100/fcastillo/RESULT/"
+snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
+
+for n in range(6):
+    for i in range(5):
+
+        input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
+
+
+        fichier = open(f"connect_{n}_{i}.sh","w")
+        fichier.write(f"""#!/bin/bash
+#SBATCH --job-name=fil_{n}_{i}
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=100gb
+#SBATCH --time=2:00:00
+#SBATCH --output=/home/fcastillo/connect_{n}_{i}.out
+#SBATCH --mail-user=fabien.castillo@etu.unistra.fr
+#SBATCH --mail-type=ALL
+#SBATCH --partition=pscomp
+""")
+        fichier.write("""
+
+module () {
+eval $(/usr/bin/modulecmd bash $*)
+}
+
+source /etc/profile.d/modules.sh
+""")
+
+        fichier.write(f"""
+module purge
+module load intelpython
+module load inteloneapi/2025.0.1
+""")
+
+
+        fichier.write(f"""
+python connect_filaments.py {n} {i}
+""")
+            
+        fichier.write("exit 0")
+        fichier.close()
+        os.system(f"sbatch connect_{n}_{i}.sh")
