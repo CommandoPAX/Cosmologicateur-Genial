22b13334f5226be65fdc956723802b0a73330571
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-28 13:09:09 +0100

    n -> N

--- connect_filaments.py
+++ connect_filaments.py
@@ -2,17 +2,17 @@ from skeletonnateur_hpc import*
 import sys
 import numpy as np
 
-n = int(sys.argv[1])
+N = int(sys.argv[1])
 i = int(sys.argv[2])
 
 snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
 labels = ["LCDM", "fnl = -500", "m = 500 eV", "WDM & fnl = -500", "fnl = 500", "WDM & fnl = 500"]
 
 
-squelette = Squelette_3d(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1.up.NDskl.S001.a.NDskl")
+squelette = Squelette_3d(f"/data100/fcastillo/RESULT/{snapshots[N]}/{i}_densite_0_c0.1.up.NDskl.S001.a.NDskl")
 
 n = []
 for p in squelette.Pointscrit :
     n.append(p.nfil)
 
-np.save(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1_connect_fil.txt", np.array(n))
+np.save(f"/data100/fcastillo/RESULT/{snapshots[N]}/{i}_densite_0_c0.1_connect_fil.txt", np.array(n))
