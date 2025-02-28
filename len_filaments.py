55e4748017f65523a5f25c901e6b80f83d5eed1f
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-02-28 09:58:09 +0100

    3d...

--- len_filaments.py
+++ len_filaments.py
@@ -9,7 +9,7 @@ snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminu
 labels = ["LCDM", "fnl = -500", "m = 500 eV", "WDM & fnl = -500", "fnl = 500", "WDM & fnl = 500"]
 
 
-squelette = Squelette_2d(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1.up.NDskl.S001.a.NDskl")
+squelette = Squelette_3d(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1.up.NDskl.S001.a.NDskl")
 
 longueurs = []
 for fil in squelette.liste_filaments :
