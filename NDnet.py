b872b5ccf42daa06479aa9c9573aa4bc188398ed
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-03 15:46:09 +0100

    source

--- NDnet.py
+++ NDnet.py
@@ -6,6 +6,7 @@ class Simplexe ():
     def __init__ (self, ndim, points) :
         self.ndim = ndim
         self.points = points
+        self.source = 0
 
     def Voulme (self) :
         pass
@@ -23,13 +24,13 @@ class Simplexe ():
                              AB[2]*AC[0] - AC[2]*AB[0],
                              AB[0]*AC[1] - AB[1]*AC[0]])
             
-            return sqrt(vect[0]**2 + vect[1]**2 + vect[2] **2)
+            return 0.5 * sqrt(vect[0]**2 + vect[1]**2 + vect[2] **2)
 
 class Vertex ():
     def __init__(self, x, y, z):
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
+        self.x = float(x) * 500/256
+        self.y = float(y) * 500/256
+        self.z = float(z) * 500/256
 
 class Net ():
     def __init__ (self, path) :
@@ -49,6 +50,7 @@ class Net ():
             self.list_vertices.append(Vertex(coords[0], coords[1], coords[2]))
 
         self.liste_simplexes = []
+        t = 0
 
         while True :
             ligne = fichier.readline()
@@ -56,12 +58,23 @@ class Net ():
             data_simplex = (ligne[:-1]).split(" ")
             T = data_simplex[0]
             N = int(data_simplex[1])
+            self.liste_simplexes.append([])
 
             for i in range(N):
                 points = (fichier.readline()[:-1]).split(" ")
-                self.liste_simplexes.append(Simplexe(int(T),[self.list_vertices[int(points[0])],
-                                                             self.list_vertices[int(points[1])],
-                                                             self.list_vertices[int(points[2])]]))
+                vertices = []
+                for p in points :
+                    vertices.append(self.list_vertices[int(p)])
+                self.liste_simplexes[t].append(Simplexe(int(T)),vertices)
+            t += 1
+
+        while True :
+            name = fichier.readline()
+            if name == "" : break
+            T = int(fichier.readline()[:-1])
+            if T == 2 :
+                for s in range(len(self.liste_simplexes[T-2])):
+                    self.liste_simplexes[T-2][s].source = int(fichier.readline()[:-1])
 
         fichier.close()
 
@@ -76,8 +89,14 @@ if __name__ == "__main__" :
     reseau = Net(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1_manifolds_J01a.NDnet.S001.a.NDnet")
 
     surfaces = []
-    for triangle in reseau.liste_simplexes :
-        surfaces.append(triangle.Surface())
+    surfaces_t = []
+    indice_p = ""
+    for triangle in reseau.liste_simplexes[0] :
+        indice = triangle.source
+        if indice != indice_p :
+            surfaces.append(np.sum(np.array(surfaces_t)))
+            surfaces_t = []
+        surfaces_t.append(triangle.Surface())
 
     np.save(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_0_c0.1_surf_murs.txt", np.array(surfaces))
 
