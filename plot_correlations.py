a14604645fbb966bc2b763213aadabdf1c794dc6
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-10 14:17:13 +0100

    0

--- plot_correlations.py
+++ plot_correlations.py
@@ -54,7 +54,7 @@ if __name__ == "__main__" :
 
                         try:
                             zeta = np.load(f"/data100/fcastillo/RESULT/extrema/snapshot_{j}_{i}_zeta_{a}_{b}_s{R}.txt.npy")
-                            print(zeta)
+                            zeta[0] = 0
 
                             X = np.linspace(x0, x1,nbins-1)
 
