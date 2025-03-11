87e034d68bab48b7c68f0a8b2374cc0d839e0789
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-11 16:59:26 +0100

    2

--- extrema_threshold.py
+++ extrema_threshold.py
@@ -57,7 +57,7 @@ pre = "/data100/fcastillo/RESULT/"
 snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
 data = pre + snapshots[n]+"/"+str(i)+"_densite.fits"
 
-R = 1
+R = 2
 
 try : 
     result = np.load(f"/data100/fcastillo/RESULT/extrema/extrema_{n}_{i}_{R}.txt.npy")
