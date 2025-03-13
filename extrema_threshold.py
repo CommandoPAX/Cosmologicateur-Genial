ce78b78c4b32d5fcb51bc47253e05b6437b83b3b
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-13 14:16:48 +0100

    test

--- extrema_threshold.py
+++ extrema_threshold.py
@@ -133,11 +133,12 @@ for j in range(4):
     seuil_haut = np.percentile(field, 95)  # 5% des points les plus hauts
     seuil_bas = np.percentile(field, 5)    # 5% des points les plus bas
 
+    """
     # Sélection des points en fonction de leur rareté
     if j in (0, 1):  # Peaks et filaments : ν > seuil_haut
         densites_interpolees = densites_interpolees[densites_interpolees >= seuil_haut]
-    elif j in (2, 3):  # Vides et murs : ν < seuil_bas
-        densites_interpolees = densites_interpolees[densites_interpolees <= seuil_bas]
+    elif j in (2, 3):  # Vides et murs : ν < seuil_bas"""
+    if True :    densites_interpolees = densites_interpolees[(densites_interpolees <= seuil_bas) | (densites_interpolees >= seuil_haut)]
 
     print(len(type_t), len(densites_interpolees))
 
