6eb16d6cd20a774f10b39a8a1b4fc0995dc02c04
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-20 10:19:41 +0100

    boucle

--- minkowski.py
+++ minkowski.py
@@ -7,29 +7,31 @@ import sys
 from astropy.io import fits
 
 n = int(sys.argv[1])
-i = int(sys.argv[2])
+#i = int(sys.argv[2])
 
-print(n, i)
+for i in range(5):
+        
+    print(n, i)
 
-pre = "/data100/fcastillo/RESULT/"
-#snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
-snapshots = ["G_ViVi","NG_Fminus500_ViVi","NG_ViVi"]
-
-
-data = pre + snapshots[n]+"/"+str(i)+"_densite.fits"
-
-hdul = fits.open(data)
-data = hdul[0].data
-hdul.close()
-print(np.shape(data))
+    pre = "/data100/fcastillo/RESULT/"
+    #snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
+    snapshots = ["G_ViVi","NG_Fminus500_ViVi","NG_ViVi"]
 
 
-v0,v1,v2,v3 = calculateMFs(data)
+    data = pre + snapshots[n]+"/"+str(i)+"_densite.fits"
 
-result = np.array([v0,v1,v2,v3])
-print(np.shape(result))
-np.save(f"{pre+snapshots[n]}/{i}_minkowski.txt", result)
-print("ok")
+    hdul = fits.open(data)
+    data = hdul[0].data
+    hdul.close()
+    print(np.shape(data))
+
+
+    v0,v1,v2,v3 = calculateMFs(data)
+
+    result = np.array([v0,v1,v2,v3])
+    print(np.shape(result))
+    np.save(f"{pre+snapshots[n]}/{i}_minkowski.txt", result)
+    print("ok")
 
 """
 plt.plot(v0,label="v0")
