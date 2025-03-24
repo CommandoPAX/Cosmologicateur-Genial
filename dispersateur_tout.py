d99b940b094769b96a15cd360d4a471a1960df72
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-24 10:20:23 +0100

    test

--- dispersateur_tout.py
+++ dispersateur_tout.py
@@ -6,22 +6,17 @@ import time
 
 pre = "/data100/fcastillo/RESULT/"
 snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500"]
-for n in range(6):
-    for i in range(5):
-
-        input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
-
-
-        fichier = open(f"../bash/mse_{n}_{i}.sh","w")
-        fichier.write(f"""#!/bin/bash
+for n in range(3):
+    fichier = open(f"../bash/mse_{n}.sh","w")
+    fichier.write(f"""#!/bin/bash
 #SBATCH --job-name=skl2_{n}_{i}
 #SBATCH --partition=pscomp
 #SBATCH --mem=500gb
 #SBATCH --time=48:00:00
-#SBATCH --output=/home/fcastillo/logs/skl2_{n}_{i}_%j.out
+#SBATCH --nodelist=i{23+n}
+#SBATCH --output=/home/fcastillo/logs/skl2_{n}.out
 """)
-        fichier.write("""
-
+    fichier.write("""
 module () {
   eval $(/usr/bin/modulecmd bash $*)
 }
@@ -30,21 +25,33 @@ source /etc/profile.d/modules.sh
 module purge
 module load disperse/0.9.24""")
 
+    for i in range(5):
+
+        input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
+
+
+
 
         fichier.write(f"""
 path_dat="{pre+snapshots[n]}/"
 path="{pre+snapshots[n]}/"
 
-file="{i}_densite"
+file="{i}_densite_smooth2"
 
 persistence=0.1
 smoothing=1
 
 /softs/disperse/0.9.24/bin/mse $path_dat$file".fits" -cut $persistence -upSkl -manifolds -outName $path$file -periodicity 0 -forceLoops
-
 /softs/disperse/0.9.24/bin/skelconv $path$file"_c"$persistence".up.NDskl" -outName $path$file"_c"$persistence".up.NDskl" -smooth $smoothing -to NDskl_ascii
+
+/softs/disperse/0.9.24/bin/mse $path_dat$file".fits" -cut $persistence -loadMSC $path$file".MSC" -dumpManifolds J0a -upSkl -manifolds -outName $path$file -periodicity 0 -forceLoops
+/softs/disperse/0.9.24/bin/netconv $path$file"_c"$persistence"_manifolds_J0a.NDnet" -outName $path$file"_c"$persistence"_manifolds_J0a.NDnet" -smooth $smoothing -to NDnet_ascii
+
+/softs/disperse/0.9.24/bin/mse $path_dat$file".fits" -cut $persistence -loadMSC $path$file".MSC" -dumpManifolds J1a -upSkl -manifolds -outName $path$file -periodicity 0 -forceLoops
+/softs/disperse/0.9.24/bin/netconv $path$file"_c"$persistence"_manifolds_J1a.NDnet" -outName $path$file"_c"$persistence"_manifolds_J1a.NDnet" -smooth $smoothing -to NDnet_ascii
+
 
 exit 0""")
             
-        fichier.close()
-        #os.system(f"sbatch ./bash/mse_{n}_{i}.sh")
+    fichier.close()
+    #os.system(f"sbatch ./bash/mse_{n}_{i}.sh")
