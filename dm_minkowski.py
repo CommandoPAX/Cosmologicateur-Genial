f63cb03d6fca751e288cbdb7b66e839699d8264f
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-25 14:28:12 +0100

    corr

--- dm_minkowski.py
+++ dm_minkowski.py
@@ -80,7 +80,7 @@ if __name__ == "__main__" :
                 axes = axs[(place-1)+(max(i,3))*8]
 
                 if d == 0 : 
-                    axes.title.set_text (r"$z = $"+str(Redshifts[i]))
+                    axes.title.set_text (rf"$v_{p} z = $"+str(Redshifts[i]))
 
 
                 for j in range(7):
