cf60ae8e4abe8ece7146916ac84e3576a5b4235d
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-26 14:04:20 +0100

    graphes

--- GRAPHES/pdfpng.py
+++
@@ -1,8 +0,0 @@
-import os
-from pdf2image import convert_from_path
-from wand.image import Image
-
-for root, dirs, files in os.walk("./WDM"):
-    for file in files:
-        with Image(filename=root+"/"+file) as img:
-            if ".pdf" in file : img.save(filename="./PNG/"+file[:-4]+".png")
