ca273b7c59135be32f418b9b1b6b529a6bb2b471
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-03-31 09:56:43 +0200

    connect_halos

---
+++ connecteur_halos.py
@@ -0,0 +1,18 @@
+from skeletonnateur_hpc import*
+import sys
+import numpy as np
+
+N = int(sys.argv[1])
+i = int(sys.argv[2])
+
+snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500", "G_ViVi", "NG_ViVi", "NG_Fminus500_ViVi"]
+labels = ["LCDM", "fnl = -500", "m = 500 eV", "WDM & fnl = -500", "fnl = 500", "WDM & fnl = 500"]
+
+
+squelette = Squelette_3d(f"/data100/fcastillo/RESULT/{snapshots[N]}/{i}_halos_s1.up.NDskl.S001.a.NDskl")
+
+n = []
+for p in squelette.Pointscrit :
+    if int(p.type) == 3: n.append(p.nfil)
+
+np.save(f"/data100/fcastillo/RESULT/{snapshots[N]}/{i}_halos_s1_connect_fil.txt", np.array(n))
