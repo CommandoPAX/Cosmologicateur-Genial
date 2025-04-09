5389a0187ab809e43b76a455b8bcff30739cf12c
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-04-09 14:18:34 +0200

    array

--- plot_connect.py
+++ plot_connect.py
@@ -178,6 +178,9 @@ if __name__ == "__main__" :
 
         for d in range(2):
 
+            moyenne = np.array(moyenne)
+            moyenne_lcdm = np.array(moyenne_lcdm)
+
             #plt.scatter(np.log(np.array([32,3,1,0.25,0][:len(moyennes)])), moyennes, color=couleur)
             if d == 0 : axs[d].errorbar(np.log10(1+np.array([3,1,0.25,0])), moyennes,ls=ls, color=couleur, label=label, yerr = err)
             if d == 1 : axs[d].errorbar(np.log10(1+np.array([3,1,0.25,0])), (moyennes-moyennes_lcdm)/moyennes_lcdm,ls=ls, color=couleur, label=label, yerr = err)
