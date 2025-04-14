e699302a823aba750a9b0eb08fd96b04f82d01f9
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-04-14 13:28:48 +0200

    &

--- plot_filaments.py
+++ plot_filaments.py
@@ -170,7 +170,11 @@ if __name__ == "__main__" :
             if d == 1 : axs[d].errorbar(np.array([3,1,0.25,0]), (moyennes-moyennes_lcdm)/moyennes_lcdm,ls=ls, color=couleur, label=label, yerr = err_ratio)
 
             if d == 0 : 
-                print(snapshots[i]+" & " +str(100*np.round((moyennes-moyennes_lcdm)/moyennes_lcdm,3)))
+                print(snapshots[i]+" & ",end="")
+                for z in [0,1,3]:
+                    print(str(100*np.round((moyennes-moyennes_lcdm)/moyennes_lcdm,3)[z]),end="")
+                    if not z == 3 : print(" & ",end="")
+                    else : print(" \\")
 
             axes.set_xlabel(r"$z$")
             if d == 0 : axs[d].set_ylabel(r"${\rm Mean~length}~/~V~~[{\rm Mpc / h}]^{-2}$")
