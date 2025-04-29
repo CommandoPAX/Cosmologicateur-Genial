246b8320e43574d67bd7fb3cd40182b02ea2db27
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-04-29 11:20:00 +0200

    tab2

--- tableaux.py
+++ tableaux.py
@@ -8,8 +8,8 @@ snapshots = ["benchM", "G_ViVi","NG_F500", "NG_Fminus500","NEDE"]
 labels = [r"$\Lambda$CDM", r"${\rm mixed~DM}$",  r"$f_{\rm NL} = -500$", r"$f_{\rm NL} = 500$ ", r"${\rm EDE}$"]
 
 
-#snapshots = ["benchM", "NsPNG_EDE_F500","NsPNG_EDE_F1833", "NG_ViVi","NG_Fminus500_ViVi"]
-#labels = [r"$\Lambda {\rm CDM}$", r"$f_{\rm NL} = -300~\&~ \rm{EDE}$",  r"$f_{\rm NL} = -1100~\&~{\rm EDE}$", r"$f_{\rm NL} = -500~\&~{\rm mixed~DM}$", r"$f_{\rm NL} = 500~\&~{\rm mixed~DM}$"]
+snapshots = ["benchM", "NsPNG_EDE_F500","NsPNG_EDE_F1833", "NG_ViVi","NG_Fminus500_ViVi"]
+labels = [r"$\Lambda {\rm CDM}$", r"$f_{\rm NL} = -300~\&~ \rm{EDE}$",  r"$f_{\rm NL} = -1100~\&~{\rm EDE}$", r"$f_{\rm NL} = -500~\&~{\rm mixed~DM}$", r"$f_{\rm NL} = 500~\&~{\rm mixed~DM}$"]
 
 plt.figure(figsize= (10,4))
 
@@ -185,12 +185,12 @@ print(tab_minko)
 ##### Fonctions de correlations
 
 
-snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500", "G_ViVi","NG_ViVi" , "NG_Fminus500_ViVi","NEDE", "NsPNG_EDE_F500"]
+snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500", "G_ViVi","NG_ViVi" , "NG_Fminus500_ViVi","NEDE", "NsPNG_EDE_F500", "NsPNG_EDE_F1833"]
 labels = [r"$\Lambda$CDM", r"$f_{\rm NL} = -500$", "m = 500 eV", "WDM & fnl = -500", r"$f_{\rm NL} = 500$", "WDM & fnl = 500", r"$m_{\rm WDM} = 10~{\rm eV}, f_{\rm WDM} = 2~\%$", r"$f_{\rm NL} = -500~\&~{\rm mixed~DM}$", r"$f_{\rm NL} = 500~\&~{\rm mixed~DM}$", r"${\rm EDE}$", r"$f_{\rm NL} = -300~\&~{\rm EDE}$", r"$f_{\rm NL} = -1100~\&~{\rm EDE}$"]
 
-#indices_hdm = [7,8,10,11]
+indices_hdm = [7,8,10,11]
 #indices_hdm = [0,2,6]
-indices_hdm = [1,4,6,9]
+#indices_hdm = [1,4,6,9]
 
 ##### Autocorrelations
 
@@ -318,6 +318,25 @@ for z in range(2):
             ls = "-"
             couleur = "darkred"
             label = r"${\rm EDE}$" 
+
+        if s == "NG_ViVi":
+            couleur = "darkorange"
+            label = r"$f_{\rm NL} = -500~\&~{\rm mixed~DM}$"
+            ls = "--"
+
+        if s == "NsPNG_EDE_F500":
+            label = r"$f_{\rm NL} = -300~\&~ \rm{EDE}$"
+            ls = ":" 
+            couleur = "darkred"
+
+        if s == "NsPNG_EDE_F1833":
+            label = r"$f_{\rm NL} = -1100~\&~{\rm EDE}$"
+            couleur = "darkred"
+            ls = "--"
+        if s == "NG_Fminus500_ViVi":
+            couleur="violet"
+            label = r"$f_{\rm NL} = 500~\&~{\rm mixed~DM}$"
+            ls = "--"
 
         axes = plt.gca()
         axes.title.set_text(r"$z = "+["1","0"][z]+r"$")
