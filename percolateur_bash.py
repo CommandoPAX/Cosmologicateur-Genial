a11be196c1b2c1dda723ed6802b2ecdd6f4169fb
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-04-29 14:56:47 +0200

    autres

--- percolateur_bash.py
+++ percolateur_bash.py
@@ -6,14 +6,14 @@ import time
 
 pre = "/data100/fcastillo/RESULT/"
 
-snapshots = ["NEDE","NsPNG_F500","NsPNG_F1000","NsPNG_F1833","NsPNG_EDE_F500","NsPNG_EDE_F1000","NsPNG_EDE_F1833"]
+snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500", "G_ViVi","NG_ViVi" , "NG_Fminus500_ViVi","NEDE", "NsPNG_EDE_F500","NsPNG_EDE_F1833"]
 
 z= [15,12, 10, 8, 5,3,1,0.5,0.25,0]
 indices_z = [5,6,8,9]
 
 
-for n in range(7):
-    for i in indices_z:
+for n in range(8): # 7
+    for i in range(5):#indices_z:
 
         #input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
 
