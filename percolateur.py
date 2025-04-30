cbba99d7aca610957ae289b336188a35d6fbd6f1
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-04-30 14:04:44 +0200

    peaks

--- percolateur.py
+++ percolateur.py
@@ -1,6 +1,7 @@
 from skeletonnateur_hpc import*
 import sys
 import numpy as np
+from astropy.io import fits
 
 from scipy.spatial import KDTree
 
@@ -38,15 +39,45 @@ snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminu
 z= [15,12, 10, 8, 5,3,1,0.5,0.25,0]
 indices_z = [5,6,8,9]
 
+snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500", "G_ViVi","NG_ViVi" , "NG_Fminus500_ViVi","NEDE", "NsPNG_EDE_F500","NsPNG_EDE_F1833"]
 
 
-squelette = Squelette_3d(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_smooth2_c0.1.up.NDskl.S001.a.NDskl")
-xyz = np.array([[p.x, p.y, p.z] for p in squelette.Pointscrit])
+R = 2
+P = 5
+
+result = np.load(f"/data100/fcastillo/RESULT/extrema/extrema_{n}_{i}_{R}.txt.npy")
+
+pre = "/data100/fcastillo/RESULT/"
+
+data = pre + snapshots[n]+"/"+str(i)+"_densite_smooth2.fits"
+
+hdul = fits.open(data)
+field = hdul[0].data
+hdul.close()
+
+
+
+X, Y, Z = result[:, 0].astype(int), result[:, 1].astype(int), result[:, 2].astype(int)
+
+
+seuil_haut = np.percentile(field[X,Y,Z], P)
+
+indices = field[X, Y, Z] >= seuil_haut
+
+
+result = result[indices]
+xyz = result[result[:,3]==0][:,0:2]
+
+
+#squelette = Squelette_3d(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_smooth2_c0.1.up.NDskl.S001.a.NDskl")
+#xyz = np.array([[p.x, p.y, p.z] for p in squelette.Pointscrit])
 taille = []
 taille2 = []
 
+Ntot = len(result)
+
 for b in np.arange(0.6,1.4,0.01) :
-    clusters = identify_clusters(xyz, b * (500**3/len(squelette.Pointscrit))**(1/3))
+    clusters = identify_clusters(xyz, b * (500**3/Ntot)**(1/3))
     taille.append(max(len(cluster) for cluster in clusters))
 
     clusters_sorted = sorted(clusters, key=len, reverse=True)
@@ -55,5 +86,5 @@ for b in np.arange(0.6,1.4,0.01) :
         deuxieme = len(clusters_sorted[1])
         taille2.append(deuxieme)
 
-np.save(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_smooth2_c0.1_percolation.txt", np.array(taille)/len(squelette.Pointscrit))
-np.save(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_smooth2_c0.1_percolation_2.txt", np.array(taille2)/len(squelette.Pointscrit))
+np.save(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_smooth2_c0.1_percolation.txt", np.array(taille)/Ntot)
+np.save(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_smooth2_c0.1_percolation_2.txt", np.array(taille2)/Ntot)
--- percolateur_bash.py
+++ percolateur_bash.py
@@ -6,13 +6,16 @@ import time
 
 pre = "/data100/fcastillo/RESULT/"
 
-snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500", "G_ViVi","NG_ViVi" , "NG_Fminus500_ViVi"]
+snapshots = ["benchM","NG_F500","G_m500","NG_F500_m500","NG_Fminus500","NG_Fminus500_m500", "G_ViVi","NG_ViVi" , "NG_Fminus500_ViVi","NEDE", "NsPNG_EDE_F500","NsPNG_EDE_F1833"]
 
 z= [15,12, 10, 8, 5,3,1,0.5,0.25,0]
 indices_z = [5,6,8,9]
 
+#indices_hdm = [0,7,8,10,11]
+#indices_hdm = [0,2,6]
+indices_hdm = [0,1,4,6,9]
 
-for n in range(9): # 7
+for n in indices_hdm: # 7
     for i in range(5):#indices_z:
 
         #input_ = pre + snapshots[n]+"/"+str(i)+"_densite"
