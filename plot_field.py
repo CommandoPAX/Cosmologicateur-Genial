ddf659478a62654d8df48ec0f7b32e364bb7291c
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-04-30 16:26:16 +0200

    axis

--- plot_field.py
+++ plot_field.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from astropy.io import fits
+from matplotlib import gridspec
+
 
 
 matplotlib.rcParams['text.usetex'] = True
@@ -21,6 +23,18 @@ indices_hdm = [0,1,4,6,7,8,9,10,11]
 Redshifts = [3,1,0.25,0]
 pre = "/data100/fcastillo/RESULT/"
 
+outer = gridspec.GridSpec(nrows=1, ncols=1)
+
+plt.figure(figsize=(9,9))
+
+
+axs = []
+for row in range(3):
+    for col in range(3):
+        inner = gridspec.GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=outer[row, col], hspace=0)
+        axs += [plt.subplot(cell) for cell in inner]
+
+
 for i in range(4):
     k = 0
 
@@ -37,7 +51,6 @@ for i in range(4):
     z1 = mean_ +2*std_
 
 
-    plt.figure(figsize=(9,9))
     plt.title(r"$z = "+str(Redshifts[i])+r"$")
     for n in indices_hdm: 
         k +=1 
@@ -45,8 +58,7 @@ for i in range(4):
         else : redshifts = indices_z
         z = redshifts[i]
         print(z)
-        plt.subplot(int("33"+str(k)))
-        axes = plt.gca()
+        axes = axs[k]
         axes.title.set_text(labels[n])
 
 
@@ -62,7 +74,9 @@ for i in range(4):
         else :
             im = axes.imshow(np.sum(field,axis=0), origin="lower",vmin = z0, vmax = z1)
 
-        plt.colorbar(im, ax=axes)  
+        if (k+1) % 3 == 0 : plt.colorbar(im, ax=axes)
+        if not k > 5 : axes.xaxis.set_visible(False)  
+        if not k % 3 == 0 : axes.yaxis.set_visible(False)  
 
         axes.set_xlim(0,500)
         axes.set_ylim(0,500)
