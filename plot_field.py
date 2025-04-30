17554f523c059e6af90d01ee00a1b86ae7c20143
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-04-30 16:20:43 +0200

    0

--- plot_field.py
+++ plot_field.py
@@ -60,7 +60,7 @@ for i in range(4):
         if n < 8:
             im = axes.imshow(np.sum(field,axis=2), origin="lower",vmin = z0, vmax = z1)
         else :
-            im = axes.imshow(np.sum(field,axis=1), origin="lower",vmin = z0, vmax = z1)
+            im = axes.imshow(np.sum(field,axis=0), origin="lower",vmin = z0, vmax = z1)
 
         plt.colorbar(im, ax=axes)  
 
