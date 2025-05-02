633049f2cf8b07c25ce02fbc49c326536c78eff9
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-05-02 09:50:52 +0200

    Xmax

--- plot_field.py
+++ plot_field.py
@@ -25,74 +25,75 @@ pre = "/data100/fcastillo/RESULT/"
 
 
 
-for i in range(4):
+for Xmax in [10,50,100,500]:
+    for i in range(4):
 
-    outer = gridspec.GridSpec(nrows=3, ncols=3)
+        outer = gridspec.GridSpec(nrows=3, ncols=3)
 
-    plt.figure(figsize=(9,9))
-
-
-    axs = []
-    for row in range(3):
-        for col in range(3):
-            inner = gridspec.GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=outer[row, col], hspace=0)
-            axs += [plt.subplot(cell) for cell in inner]
+        plt.figure(figsize=(9,9))
 
 
-    k = 0
-
-    data = pre + snapshots[0]+"/"+str(i+1)+"_densite_smooth2.fits"
-
-    hdul = fits.open(data)
-    lcdm = hdul[0].data
-    hdul.close()
-
-    sum_ = np.sum(lcdm[0:100,0:100,0:2],axis=2)
-    mean_ = np.mean(sum_)
-    std_ = np.std(sum_)
-    z0 = sum_.min()
-    z1 = sum_.max()
-    print(np.argmin(sum_))
-    print(sum_)
-    rho_m = 1073741824000000
-    mass = rho_m * sum_ + rho_m
-    
+        axs = []
+        for row in range(3):
+            for col in range(3):
+                inner = gridspec.GridSpecFromSubplotSpec(nrows=1, ncols=1, subplot_spec=outer[row, col], hspace=0)
+                axs += [plt.subplot(cell) for cell in inner]
 
 
-    plt.title(r"$z = "+str(Redshifts[i])+r"$")
-    for n in indices_hdm: 
-        k +=1 
-        if n <= 8 : redshifts = range(1,5)
-        else : redshifts = indices_z
-        z = redshifts[i]
-        print(z)
-        axes = axs[k-1]
-        axes.title.set_text(labels[n])
+        k = 0
 
-
-        data = pre + snapshots[n]+"/"+str(z)+"_densite_smooth2.fits"
+        data = pre + snapshots[0]+"/"+str(i+1)+"_densite_smooth2.fits"
 
         hdul = fits.open(data)
-        field = hdul[0].data
+        lcdm = hdul[0].data
         hdul.close()
 
-        sum_ = np.sum(field[0:100,0:100,0:2],axis=2)
+        sum_ = np.sum(lcdm[0:Xmax,0:Xmax,0:2],axis=2)
+        mean_ = np.mean(sum_)
+        std_ = np.std(sum_)
+        z0 = sum_.min()
+        z1 = sum_.max()
+        print(np.argmin(sum_))
+        print(sum_)
+        rho_m = 1073741824000000
+        mass = rho_m * sum_ + rho_m
+        
 
 
-        if True:
-            im = axes.imshow(sum_-sum_.min()+0.1, origin="lower", norm=LogNorm(vmin=0.1,vmax=z1-z0+0.1))
+        plt.title(r"$z = "+str(Redshifts[i])+r"$")
+        for n in indices_hdm: 
+            k +=1 
+            if n <= 8 : redshifts = range(1,5)
+            else : redshifts = indices_z
+            z = redshifts[i]
+            print(z)
+            axes = axs[k-1]
+            axes.title.set_text(labels[n])
 
 
-        #if (k) % 3 == 0 : plt.colorbar(im)
-        if not k-1 > 5 : axes.xaxis.set_visible(False)  
-        if not (k-1) % 3 == 0 : axes.yaxis.set_visible(False)  
+            data = pre + snapshots[n]+"/"+str(z)+"_densite_smooth2.fits"
 
-        #axes.set_xlim(0,50)
-        #axes.set_ylim(0,50)
+            hdul = fits.open(data)
+            field = hdul[0].data
+            hdul.close()
 
-        axes.set_xlabel(r"$\rm X [Mpc / h]$")
-        axes.set_ylabel(r"$\rm Y [Mpc / h]$")
+            sum_ = np.sum(field[0:Xmax,0:Xmax,0:2],axis=2)
 
-    plt.tight_layout()
-    plt.savefig(f"field_{Redshifts[i]}.pdf")
-    plt.clf()
+
+            if True:
+                im = axes.imshow(sum_-sum_.min()+0.1, origin="lower", norm=LogNorm(vmin=0.1,vmax=z1-z0+0.1))
+
+
+            #if (k) % 3 == 0 : plt.colorbar(im)
+            if not k-1 > 5 : axes.xaxis.set_visible(False)  
+            if not (k-1) % 3 == 0 : axes.yaxis.set_visible(False)  
+
+            #axes.set_xlim(0,50)
+            #axes.set_ylim(0,50)
+
+            axes.set_xlabel(r"$\rm X [Mpc / h]$")
+            axes.set_ylabel(r"$\rm Y [Mpc / h]$")
+
+        plt.tight_layout()
+        plt.savefig(f"field_{Redshifts[i]}_{Xmax}.pdf")
+        plt.clf()
