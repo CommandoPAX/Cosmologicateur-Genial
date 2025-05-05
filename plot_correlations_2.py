744f907116e6965e4c6654a5154cf6a26fc46410
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-05-05 10:06:05 +0200

    ylim

--- plot_correlations_2.py
+++ plot_correlations_2.py
@@ -340,8 +340,10 @@ if __name__ == "__main__" :
                             axes.legend(fontsize=8) 
                         axes.set_xlim(0,20)
                         if p ==2 : 
-                            axes.set_xlim(2,20)
-                            axes.set_ylim(1,1.5)
+                            axes.set_xlim(2,15)
+                            if i == 2 and d == 0:axes.set_ylim(1,1.5)
+                            if i == 2 and d == 0 :axes.set_ylim(1,1.2)
+
                             #if d == 1 : axes.set_ylim(-0.1,0.4)
 
                         if d == 1 : 
