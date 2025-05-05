af49463b04a3d9b311041f8fdb0825af3969ecda
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-05-05 14:11:01 +0200

    1+

--- tableaux.py
+++ tableaux.py
@@ -214,7 +214,7 @@ for p in range(4):
             try : zeta = np.load(f"/data100/fcastillo/RESULT/extrema/snapshot_{i}_{j}_zeta_{p}_{p}_s{R}_P{P}_tres_grand.txt.npy")
             except : zeta = np.load(f"/data100/fcastillo/RESULT/extrema/snapshot_{i}_{z_k}_zeta_{p}_{p}_s{R}_P{P}_tres_grand.txt.npy")
 
-            max_delta = np.argmax(np.abs(zeta))
+            max_delta = np.argmax(np.abs(1+zeta))
 
             tab_corr +=" &"+str(round(100*((zeta[max_delta]-lcdm[max_delta])/(np.max(np.abs(1+lcdm)))),1))+ r" \%"
                 
