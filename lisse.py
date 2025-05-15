47bf70f5517a2be352c2e54f34378d1ccafa61cc
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-05-15 10:46:11 +0200

    lisse

--- lisse.py
+++ lisse.py
@@ -3,11 +3,11 @@ import numpy as np
 from py_extrema.extrema import ExtremaFinder, CriticalPoints
 
 pre = "/data100/fcastillo/RESULT/"
-snapshots = ["NBM"]
+snapshots = ["SIM0N_BIG","SIM1N_BIG","SIM2N_BIG","SIM3N_BIG"]
 
 for R in [1,2,5]:
-    for n in range(1):
-        for i in range(10):
+    for n in range(4):
+        for i in range(4):
             print(R, n, i)
             data = pre + snapshots[n]+"/"+str(i)+"_densite.fits"
             hdul = fits.open(data)
@@ -20,9 +20,9 @@ for R in [1,2,5]:
             header = fits.Header()
             header['COMMENT'] = 'Champ de densite'
             header['NAXIS'] = 3  
-            header['NAXIS1'] = 512
-            header['NAXIS2'] = 512  
-            header['NAXIS3'] = 512 
+            header['NAXIS1'] = 1024
+            header['NAXIS2'] = 1024 
+            header['NAXIS3'] = 1024
 
             hdu = fits.PrimaryHDU(data=field, header=header)
             hdu.writeto(f"/data100/fcastillo/RESULT/{snapshots[n]}/{i}_densite_smooth{R}.fits", overwrite=True)
