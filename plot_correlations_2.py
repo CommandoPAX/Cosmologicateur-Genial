b912d56fa44ceb59df86311fdcba4c74c9925fc7
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-06-05 10:48:09 +0200

    6

--- plot_correlations_2.py
+++ plot_correlations_2.py
@@ -240,7 +240,7 @@ if __name__ == "__main__" :
                         else : 
                             axes.plot(r_bins[1:], (zeta - lcdm), color=couleurs[j], ls=ls[j],label=labels[j])
                             axes.set_ylabel(r"$\Delta$")
-                            axes.set_ylim(-10,10)
+                            axes.set_ylim(-6,6)
                         if d ==1 and i == 4: axes.set_xlabel(r"$r {\rm [Mpc / h]}$")
                         if j == indices_hdm[len(indices_hdm)-1] and i == 2 and d == 0 and p == 0: 
                             pass#axes.legend(loc="upper left",fontsize=8) 
