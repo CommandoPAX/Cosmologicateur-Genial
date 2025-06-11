f0e5a8dba907aca40a9757a663a8a62f63d029f2
casse-stylo <fabien.castillo@etu.unistra.fr>
2025-06-11 12:13:42 +0200

    test

--- plot_field.py
+++ plot_field.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib
 from astropy.io import fits
 from matplotlib import gridspec
-from matplotlib.colors import LogNorm, SymLogNorm
+from matplotlib.colors import LogNorm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
@@ -85,6 +85,7 @@ for BM in range(2):
                     z = redshifts[i]
                     print(z)
                     axes = axs[k-1]
+                    axes.title.set_text(r"$ \Lambda{\rm CDM}$")
 
 
                     data = pre + snapshots[n]+"/"+str(z)+"_densite_smooth2.fits"
@@ -97,10 +98,8 @@ for BM in range(2):
 
 
                     if n > 0 and n!=12:
-                        axes.title.set_text(labels[n]+r"$ - \Lambda{\rm CDM}$")
                         im = axes.imshow(sum_-sum_lcdm, origin="lower",vmin=-0.5,vmax = 0.5,cmap="bwr")
                     else : 
-                        axes.title.set_text(r"$\Lambda{\rm CDM}$")
                         im = axes.imshow(sum_, origin="lower",cmap="viridis")
 
                     ims.append(im)
